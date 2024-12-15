c0cff3e5cad16017cf7497fbcefa144c0032e692
David Zang <36739408+DaZang@users.noreply.github.com>
2024-12-15 12:30:13 +0100

    make player only move with left mouse button; cap maximum and minimum acceleration

--- entities/characters/player.gd
+++ entities/characters/player.gd
@@ -1,9 +1,10 @@
 class_name Player
 extends CharacterBody2D
 
-@export var ACCELERATION = 1100
-@export var MAX_SPEED = 1000
-@export var FRICTION = 1100
+@export var ACCELERATION = 200
+@export var MAX_SPEED = 2
+@export var MAX_ACCELERATION_VECTOR_LENGTH = 40
+@export var FRICTION = 50
 
 var input_vector = Vector2.ZERO
 
@@ -13,14 +14,19 @@ func _ready():
 	
 	
 func _physics_process(delta):
-	var target_position = get_global_mouse_position()
-	var direction = (target_position - global_position).normalized()
-	var movement = direction * MAX_SPEED
-	#fun version of following cursor with acceleration,
-	#kinda looks like a skating snowball
-	velocity = velocity.move_toward(movement, ACCELERATION * delta)
+	#var target_position = get_global_mouse_position()
+	#var direction = (target_position - global_position).normalized()
+	#var movement = direction * MAX_SPEED
+#
+	#velocity = velocity.move_toward(movement, ACCELERATION * delta)
+	if Input.is_action_pressed("left_mouse_button"):
+		var acceleration_vector = get_global_mouse_position() - position
+		if acceleration_vector.length() < 20:
+			acceleration_vector = Vector2.ZERO
+		var capped_acceleration_multiplier = min(acceleration_vector.length(), MAX_ACCELERATION_VECTOR_LENGTH) \
+			/ MAX_ACCELERATION_VECTOR_LENGTH
+		acceleration_vector = acceleration_vector * capped_acceleration_multiplier
+		velocity = velocity.move_toward(acceleration_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move_and_slide()
-	
-	#following the cursor without acceleration
-	#if global_position.distance_to(target_position) > 10:
-		#global_position += movement
