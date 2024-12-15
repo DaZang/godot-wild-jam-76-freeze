2db4a7baa1639607a56289a1fd2defa2225def48
David Zang <36739408+DaZang@users.noreply.github.com>
2024-12-15 16:48:01 +0100

    resize noise meter with tween

--- entities/interactable_ui/noise_meter.gd
+++ entities/interactable_ui/noise_meter.gd
@@ -5,6 +5,9 @@ const BLOCK_SIZE = 16
 var start_position: Vector2
 var start_size: Vector2
 
+var new_position: Vector2
+var new_size: Vector2
+
 @onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
 @onready var sprite_2d: Sprite2D = %Sprite2D
 
@@ -12,16 +15,22 @@ var start_size: Vector2
 func _ready():
 	start_position = collision_shape_2d.position
 	start_size = collision_shape_2d.shape.get_rect().size
+	new_position = start_position
+	new_size = start_size
 	GameEvents.noise_level_changed.connect(on_noise_level_changed)
 	
 	
 func on_noise_level_changed(noise_level: int):
 	var target_position = Vector2(start_position.x, start_position.y - (noise_level/2  - BLOCK_SIZE))
 	var target_size = Vector2(start_size.x, start_size.y + noise_level - BLOCK_SIZE)
-	#var tween = create_tween()
-	#tween.tween_property(self, "")
+	var position_tween = create_tween()
+	position_tween.tween_property(self, "new_position", target_position, 0.5).set_ease(Tween.EASE_IN_OUT)
+	var size_tween = create_tween()
+	size_tween.tween_property(self, "new_size", target_size, 0.5).set_ease(Tween.EASE_IN_OUT)
 	
-	collision_shape_2d.position = target_position
-	collision_shape_2d.shape.extents = target_size / 2
-	sprite_2d.position = target_position
-	sprite_2d.region_rect.size = target_size
+	
+func _physics_process(delta: float) -> void:
+	collision_shape_2d.position = new_position
+	collision_shape_2d.shape.extents = new_size / 2
+	sprite_2d.position = new_position
+	sprite_2d.region_rect.size = new_size
