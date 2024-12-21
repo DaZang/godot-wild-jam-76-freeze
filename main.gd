f6e58778deeb5ef6bb43f1b7de5afc1171ab5c78
David Zang <36739408+DaZang@users.noreply.github.com>
2024-12-21 11:16:33 +0100

    ✨ pause game when pause menu is opened

--- main.gd
+++ main.gd
@@ -1,1 +1,15 @@
 extends Node2D
+
+@onready var frames_per_second_ui: Control = %FramesPerSecondUi
+@onready var pause_menu_ui: PauseMenuUi = %PauseMenuUi
+@onready var world: Node2D = %World
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if pause_menu_ui.visible:
+			pause_menu_ui.hide()
+			world.get_tree().paused = false
+		else:
+			pause_menu_ui.show()
+			world.get_tree().paused = true
