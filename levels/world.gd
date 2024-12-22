a01bef7cf08716acb69d130a20494aa57a6229aa
David Zang <david.zang@viadee.de>
2024-12-22 12:22:18 +0100

    💄 change screen resolution to 960 * 540; add success screen

--- levels/world.gd
+++ levels/world.gd
@@ -1,4 +1,7 @@
+class_name World
 extends Node2D
+
+signal game_completed
 
 var current_level_id = "1"
 
@@ -33,15 +36,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		#load_level("8")
 		pass
 	elif event.is_action_pressed("load_level_9"):
-		load_level("9")
+		#load_level("9")
 		pass
 		
 
 func load_level(level_id: String):
 	print("load level " + level_id)
+	
 	var level_scene = load("res://levels/level" + level_id + "/level" + level_id + ".tscn")
 	if level_scene == null:
 		print("new level scene can not be loaded")
+		game_completed.emit()
 		return
 	var old_level = get_tree().get_first_node_in_group("level")
 	if old_level != null:
--- main.gd
+++ main.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 @onready var frames_per_second_ui: Control = %FramesPerSecondUi
 @onready var pause_menu_ui: PauseMenuUi = %PauseMenuUi
-@onready var world: Node2D = %World
+@onready var world: World = %World
+@onready var hud_canvas_layer: CanvasLayer = %HudCanvasLayer
 
+const GAME_COMPLETED_MENU_SCENE = preload("res://ui/game_completed_menu.tscn")
 
 func _ready() -> void:
 	GameSettings.game_speed_setting_changed.connect(on_game_speed_setting_changed)
-
+	world.game_completed.connect(on_game_completed)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -21,3 +23,9 @@ func _input(event: InputEvent) -> void:
 
 func on_game_speed_setting_changed(value: float) -> void:
 	Engine.time_scale = value
+	
+	
+func on_game_completed() -> void:
+	var game_completed_menu =  GAME_COMPLETED_MENU_SCENE.instantiate()
+	hud_canvas_layer.call_deferred("add_child", game_completed_menu)
+	world.get_tree().paused = true
